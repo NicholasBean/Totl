96f570436263d0e6d85ef69cadefc7e804260be0
Mackenzie <84205474+MackenzieI@users.noreply.github.com>
2024-03-01 09:27:01 -0600

    Update Contact.js

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,19 +1,21 @@
 import React from "react";
 function Contact() {
   return (
+  <div className="contact-page">
     <div className="page-heading">
       <h1>Contact</h1>
-      <div>
-        <a href="https://github.com/NicholasBean/Totl/tree/main">
-          <img
-            src={"https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png"}
-            alt="Github Link"
-            title="Github Link"
-            height="100"
-          />
-        </a>
-      </div>
     </div>
+    <h4>Link to Our GitHub</h4>
+    <a className="github-link" href="https://github.com/SantiagoGuty/Totl/tree/sprint-two">
+      <img
+        src={"https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png"}
+        alt="Github Link"
+        title="Github Link"
+        height="100"
+        />
+    </a>
+    <a href="https://github.com/users/NicholasBean/projects/1/views/2"><h4>Link to Our Project</h4></a>
+  </div>
   );
 }
 
