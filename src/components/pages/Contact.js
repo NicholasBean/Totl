e1a4d423bc0314249e3b11692129ab75e697d672
Mackenzie <84205474+MackenzieI@users.noreply.github.com>
2024-02-29 19:36:27 -0600

    Update Contact.js
    Github link added

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -3,6 +3,16 @@ function Contact() {
   return (
     <div className="page-heading">
       <h1>Contact</h1>
+      <div>
+        <a href="https://github.com/NicholasBean/Totl/tree/main">
+          <img
+            src={"https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png"}
+            alt="Github Link"
+            title="Github Link"
+            height="100"
+          />
+        </a>
+      </div>
     </div>
   );
 }
