e1401f4b6681108ba9c7b8ac78bee09c4f12a580
Mackenzie <84205474+MackenzieI@users.noreply.github.com>
2024-03-01 09:27:37 -0600

    Update About.js

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,6 +3,7 @@ function About() {
   return (
     <div className="page-heading">
       <h1>About</h1>
+      <h2>A project by Nick Bean, Martha Aponbiede, and Mackenzie Albright</h2>
     </div>
   );
 }
