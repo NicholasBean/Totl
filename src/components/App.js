7fc255d526f8e89c8da5f163df265ae0fbe0c882
Martha Aponbiede <105121021+maponbie@users.noreply.github.com>
2024-04-01 17:15:28 -0500

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,47 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { useEffect } from 'react';
-
 import Home from './pages/Home';
-import Citations from './pages/Citations';
-import About from './pages/About';
+import Citations from "./pages/Citations";
+import MyLib from './pages/MyLib';
 import Contact from './pages/Contact';
-
-
 import Navbar from './Navbar/Navbar';
 
 import './App.css';
 
-
 function App() {
-  const google = window.google;
-
-  const handleCallBackResponse = (response) =>{
-    console.log('Encoded JWT ID token: ' + response.credential);
-  };
-
-  useEffect(() => {
-    google.accounts.id.initialize({
-      client_id: '410218340206-284a7b6075rag3eci4k8kje62ar7s5to.apps.googleusercontent.com',
-      callback: handleCallBackResponse,
-    });
-
-    google.accounts.id.renderButton(document.getElementById("Sign-in-div"), {
-        theme: "outline", 
-        size: "large"
-      });
-  }, []);
-
-
   return (
-    <div className="container">
-      <div id="sign-in-div"></div>
+    <div className="container"> 
       <Router>
         <Navbar />
         <Routes>
           <Route path="/" exact element={<Home />} />
-          <Route path="/citations" exact element={<Citations />} />
-          <Route path="/about" exact element={<About />} />
+          <Route path="/mylib" exact element={<MyLib />} />
+          <Route path="/citations" exact element={<Citations />} /> 
           <Route path="/contact" exact element={<Contact />} />
         </Routes>
       </Router>
