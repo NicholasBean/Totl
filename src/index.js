e61b4e1ed7ddc07f952e8d9fcf941de51fa51e1d
Martha Aponbiede <105121021+maponbie@users.noreply.github.com>
2024-04-02 14:53:06 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './components/App';
-
+import ReactDOM from 'react-dom';
+import LoginPage from './components/LoginPage'; // Assuming your LoginPage component file is located here
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <LoginPage /> {/* Render LoginPage instead of App initially */}
   </React.StrictMode>
 );
-
-
