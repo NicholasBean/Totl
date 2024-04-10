fd65c0105320610431e89eb083c3ce9f6ccd5512
Martha Aponbiede <105121021+maponbie@users.noreply.github.com>
2024-04-10 14:56:36 -0500

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,42 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Citations from "./pages/Citations";
-import MyLib from './pages/MyLib';
-import Contact from './pages/Contact';
-import Navbar from './Navbar/Navbar';
-import './App.css';
-import BGImageButton from './pages/links/BGImageButton';
+import Home from './components/pages/Home';
+import Citations from "./components/pages/Citations";
+import MyLib from './components/pages/MyLib';
+import Contact from './components/pages/Contact';
+import LoginPage from './components/pages/LoginPage';
+import BGImageButton from './components/pages/links/BGImageButton';
 import { useState } from 'react';
+
+import './App.css';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
 
   const [bgImg, setBgImg] = useState("container");
-
   return (
-    <div className={bgImg}> 
+    <div className="container"> 
       <Router>
-        <Navbar />
-        <BGImageButton setBgImg={setBgImg}/>
-
         <Routes>
-          <Route path="/" exact element={<Home />} />
-          <Route path="/mylib" exact element={<MyLib />} />
-          <Route path="/citations" exact element={<Citations />} /> 
-          <Route path="/contact" exact element={<Contact />} />
+          {/* Define login route */}
+          <Route path="/" element={<LoginPage />} />
+          {/* Define protected routes */}
+          <Route
+            path="/*"
+            element={
+              <>
+                {/* Navbar rendered for all routes except login */}
+                <Navbar />
+                <BGImageButton setBgImg={setBgImg}/>
+                <Routes>
+                  <Route path="/home" element={<Home />} />
+                  <Route path="/mylib" element={<MyLib />} />
+                  <Route path="/citations" element={<Citations />} /> 
+                  <Route path="/contact" element={<Contact />} />
+                </Routes>
+              </>
+            }
+          />
         </Routes>
       </Router>
     </div>
