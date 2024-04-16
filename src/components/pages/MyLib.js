528738b036a9671c57f6963c3c9f4243ef6f3f96
Mackenzie <84205474+MackenzieI@users.noreply.github.com>
2024-04-15 21:04:08 -0500

    Update MyLib.js

--- src/components/pages/MyLib.js
+++ src/components/pages/MyLib.js
@@ -1,28 +1,83 @@
 import React from "react";
 import MylibCitation from "./links/MylibCitation";
+import './MyLib.css';
+
+/*
+  MYLIB 
+  - FUNCTIONALITY (SELECT), (CITATION STYLE)
+  - STYLE
+  MYLIBCITATION 
+  - STYLE
+  - FUNCTIONALITY (CITATIONS)
+  RESEARCH DATABASE FOR ADDING THIS DATA TO POSTGRESQL
+  STYLES
+  - KEEP IT IN ACCOUNT
+
+
+  upload your changes to app.css and mylib.css
+*/
 
 function MyLib() {
   return (
     <div className="Mylib-page">
-      <div className="page-heading">
-        <h1>MYLib</h1>
-      </div>
+
+      <table className="mylib-table">
+        <tr>
+          <th>Author(s)</th>
+          <th>Title</th>
+          <th>Year</th>
+          <th>ISBN</th>
+          <th>Publishing Location</th>
+          <th>Pages</th>
+        </tr>
+        <tr>
+          <td>Harold Abelson, Gerald Jay Sussman, Julie Sussman</td>
+          <td>Structure and Interpretation of Computer Programs</td>
+          <td>1985</td> 
+          <td>9788320427127</td>
+          <td>Paris</td>
+          <td>595</td>
+        </tr>
+        <tr>
+          <td>Mark Lutz, David Ascher</td>
+          <td>Learning Python</td>
+          <td>1999</td> 
+          <td>1565924649</td>
+          <td>Taipei</td>
+          <td>591</td>
+        </tr>
+
+      </table>
+
+
       <div className="mylib-container">
-      <select>
-        <option>Chicago</option>
-        <option>MLA</option>
-        <option>APA</option>
-        </select>
-        <button className="copy-all-button">Copy Bibliography</button>
+        <div>
+
+          <select className="citation-style-select">
+            <option>Chicago</option>
+            <option>MLA</option>
+            <option>APA</option>
+          </select>
+          <button className="copy-all-button">Copy Bibliography</button>
+
+        </div>
+
         <MylibCitation 
-          info="Citation example one"
+          author="Harold Abelson, Gerald Jay Sussman, Julie Sussman."
+          title="Structure and Interpretation of Computer Programs (SICP)."
+          year="1985." 
+          isbn="ISBN 9788320427127." 
+          location="Paris:"
+          pages="595 pages."
         />        
         <MylibCitation 
-          info="Citation example two"
+          author="Mark Lutz, David Ascher." 
+          title="Learning Python." 
+          year="1999." 
+          isbn="ISBN 1565924649." 
+          location="Taipei:" 
+          pages="591 pages."
         />        
-      <MylibCitation 
-        info="Citation example three"
-      />
       </div>
     </div>
   );
