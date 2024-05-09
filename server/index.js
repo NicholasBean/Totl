4dd65b3f668f79c113885c9b51ee10facfbd0a57
NicholasBean <105476193+NicholasBean@users.noreply.github.com>
2024-05-09 17:03:59 -0500

    Update index.js
    Updated index w/ crud functions that work.

--- server/index.js
+++ server/index.js
@@ -1,72 +1,110 @@
 const express = require('express');
 const app = express();
-const cors = require("cors");
-const pool = require("./db");
+const cors = require('cors');
+const pool = require('./db');
+const port = 3001;
 
+
+ // middleware
 app.use(cors());
 app.use(express.json());
 
+
 // Routes //
 
-//Create a citation
-app.post("/citations", async(req, res) =>{
-    try {
-        const { citation } = req.body;
-        const newCitation = await pool.query("INSERT INTO citations (author) VALUES($1) RETURNING *", 
-        [author]
-    );
-    res.json(newCitation.rows[0]);
-    } catch (error) {
-        console.error(error.message);
-    }
+
+
+// ------------------------------------------------------ //
+
+// Create Citation for Author: //
+app.post("/cite", async(req, res) => {
+	try {
+		const { author } = req.body;
+		const newAuthor = await pool.query(
+		"INSERT INTO citations (author) VALUES($1) RETURNING *",
+		[author]);
+		res.json(newAuthor.rows[0]);
+	}
+	catch (error) {
+		console.error(error.message);
+		console.log("Error, could not post.");
+	}
+});
+
+// ------------------------------------------------------ //
+
+// Get Authors from Citations Table //
+app.get("/cite", async(req, res) => {
+	try{
+		const allAuthors = await pool.query(
+		"SELECT * FROM citations");
+		res.json(allAuthors.rows);
+	}
+	catch (error) {
+		console.error(error.message);
+		console.log("Error, could not get.");
+	}
 });
 
 
-//Get all citations
-app.get("/citations", async(req, res) =>{
-    try {
-        const allCitations = await pool.query("SELECT * FROM citations")
-        res.json(allCitations.rows);
-    } catch (error) {
-        console.error(error.message);
-    }
+// ------------------------------------------------------ //
+
+// Get a specific Author from Citations //
+app.get("/cite/:id", async(req, res) => {
+	try{
+		const { id } = req.params;
+		const author = await pool.query(
+		"SELECT * FROM citations WHERE citation_id = $1",
+		[id]);
+		res.json(author.rows[0]);
+	}
+	catch (error) {
+		console.error(error.message);
+		console.log("Error, could not get specific.");
+	}
 });
 
+// ------------------------------------------------------ //
 
-//Get specific citation via citation_id
-app.get("/citations/:id", async (req, res) =>{
-    try {
-        const { id } = req.params;
-        const cite = await pool.query("SELECT * FROM citations WHERE todo_id = $1", [id])
-        res.json(citations.rows[0]);
-    } catch (error) {
-        console.error(error.message);
-    }
+// Update author from citations //
+app.put("/cite/:id", async(req, res) => {
+	try{
+		const { id } = req.params;
+		const { author } = req.body;
+		
+		const updateAuthor = await pool.query(
+		"UPDATE citations SET author = $1 WHERE citation_id = $2",
+		[author, id]
+		);
+		res.json("Authors Updated!");
+	}
+	catch (error) {
+		console.error(error.message);
+		console.log("Error, could not put.");
+	}
 });
 
+// ------------------------------------------------------ //
 
-//Update citations
-app.put("citations/:id", async(req, res) => {
-    try {
-        const { id } = req.params;
-        const { author } = req.body;
-        const updateCitation = await pool.query("UPDATE citations SET author = $1 WHERE citation_id = $2", [author, id]);
-
-        res.json("Citation Updated.");
-    } catch (error) {
-        console.error(error.message);
-    }
+// Delete Author from Citations //
+app.delete("/cite/:id", async(req, res) => {
+	try{
+		const { id } = req.params;
+		const deleteAuthor = await pool.query(
+		"DELETE FROM citations WHERE citation_id = $1", 
+		[id]
+		);
+		res.json("Author Deleted");
+	}
+	catch (error) {
+		console.error(error.message);
+		console.log("Error, could not delete.");
+	}
+	
 });
 
+// ------------------------------------------------------ //
 
-//Delete citations
-app.delete("/citations/:id", async(req, res) =>{
-    try {
-        const { id } = req.params;
-        const deleteCitation = await pool.query("DELETE FROM citations WHERE citation_id = $1", [id]);
-        res.json("Citation deleted.");
-    } catch (error) {
-        console.error(error.message);
-    }
+app.listen(port, () => {
+	console.log("this live on", {port});
 });
-
