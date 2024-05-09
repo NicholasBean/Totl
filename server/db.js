3a98c6ba8119682b29b0b28d80432822e592ad26
NicholasBean <105476193+NicholasBean@users.noreply.github.com>
2024-05-09 17:03:10 -0500

    Update db.js
    Updated hostname.

--- server/db.js
+++ server/db.js
@@ -3,7 +3,7 @@ const Pool = require("pg").Pool;
 const pool = new Pool ({
     user: "totl",
     password: "g0valp0",
-    host: "127.0.0.1",
+    host: "10.3.158.66",
     database: "totl",
     port: 5432
 });
